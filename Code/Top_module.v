151bae861bd56c12ba4a4a4e1773f5d80e6c084e
Mohammed Riyaj <mdriyaj93454@gmail.com>
2025-08-09 10:42:21 +0530

    Update Top_module.v

--- Code/Top_module.v
+++ Code/Top_module.v
@@ -1,1 +1,7 @@
+module top_seq_dec(input  clk,input  reset,input data_in,output detected,output clkout);
+  wire c_out;
+  assign clkout=c_out;
+  clockdivider uut1(c_out,clk,reset);
+  seq_dec uut2(c_out,reset,data_in,detected);  
+endmodule
 
